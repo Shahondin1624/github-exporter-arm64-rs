5fb8e9cd3211961dc0dd9e3edd700d92d5d574da
Shahondin1624 <shahondin1624@gmail.com>
2023-06-14 16:09:28 +0200

    more metrics for commits

--- src/metrics.rs
+++ src/metrics.rs
@@ -10,10 +10,36 @@ pub fn extract_number_of_commits_per_repository(data: &RepositoriesWithCommits) 
     }).collect()
 }
 
-pub fn extract_number_of_commits(data: &RepositoriesWithCommits) -> i128 {
+pub fn extract_total_number_of_commits(data: &RepositoriesWithCommits) -> i128 {
     data.data.iter()
         .map(|repository| &repository.commits)
         .map(|commits| commits.len() as i128).sum()
+}
+
+pub fn extract_number_of_additions_per_commit(data: &RepositoriesWithCommits) -> Vec<(String, String, i128)> {
+    data.data.iter()
+        .flat_map(|repository|{
+            let repo_name = repository.repository.name.clone();
+            repository.commits.iter()
+                .map(move |commit| {
+                    let sha = commit.commit.sha.clone();
+                    let additions = commit.changes.stats.additions;
+                    (repo_name.clone(), sha.clone(), additions as i128)
+                })
+        }).collect()
+}
+
+pub fn extract_number_of_deletions_per_commit(data: &RepositoriesWithCommits) -> Vec<(String, String, i128)> {
+    data.data.iter()
+        .flat_map(|repository|{
+            let repo_name = repository.repository.name.clone();
+            repository.commits.iter()
+                .map(move |commit| {
+                    let sha = commit.commit.sha.clone();
+                    let deletions = commit.changes.stats.deletions;
+                    (repo_name.clone(), sha.clone(), deletions as i128)
+                })
+        }).collect()
 }
 
 pub fn extract_total_number_of_additions(data: &RepositoriesWithCommits) -> i128 {
